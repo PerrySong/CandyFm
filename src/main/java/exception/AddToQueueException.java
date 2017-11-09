b74decabfbd85d1f3a9502e3a941642cbdfb7785
spfscmz <psong4@dons.usfca.edu>
2017-11-09 15:50:06 -0800

    Add documentation.

--- src/main/java/exception/AddToQueueException.java
+++ src/main/java/exception/AddToQueueException.java
@@ -2,6 +2,10 @@ package exception;
 
 public class AddToQueueException extends Exception{
 	
+	/*
+	 *For objects of ExecutorService, when you call shutdown then you call execute, there will be a AddToQueueException.
+	 */
+	
 	public AddToQueueException(String msg) {
 		super(msg);
 	}
